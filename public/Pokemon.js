d2c83524c5988062d1953aab5cab091df5d0f4e5
Plainswalker995 <sageleecannon@att.net>
2019-12-19 22:42:01 -0500

    commit on master

--- public/Pokemon.js
+++ public/Pokemon.js
@@ -1,33 +1,66 @@
-const baseURL = "https://pokeapi.co/api/v2/pokemon-species/"
+const baseURL = "https://pokeapi.co/api/v2/pokemon/"
 let url;
-
-fetch("https://pokeapi.co/api/v2/pokemon-species/")
 
 const searchTerm = document.querySelector('.search');
 const startnumber = document.querySelector('.start-number');
 const endNumber = document.querySelector('.end-number')
 const submitBtn = document.querySelector('.submit')
+const searchForm = document.querySelector('form')
 
 const nextBtn = document.querySelector('.next')
 const previousBtn = document.querySelector('.prev')
-const nav = document.querySelector('section')
+const section = document.querySelector('section')
 
-nav.style.display = 'none';
-
-let pageNumber = 0;
-console.log('Page number: ', pageNumber)
+// nav.style.display = 'none';
 let displayNav = false;
 
 searchForm.addEventListener('submit', fetchResults);
-nextBtn.addEventListener('click', nextPage);
-previousBtn.addEventListener('click',previousPage);
 
 function fetchResults(e) {
     e.preventDefault();
-    url = baseURL + '&page=' + pageNumber + '&q=' + searchTerm.nodeValue;
+    url = baseURL + searchTerm.value;
     console.log('URL: ', url)
-    if(startNumber.value !== '') {
-        url += '&end_number=' + endNumber.value;
-    }
     
+    fetch(url).then(function(result) {
+        return result.json();
+    }).then(function(json) {
+        displayResults(json);
+    })
+}
+
+function displayResults(json) {
+    console.log(json)
+
+    let pokeName = json.name;
+    console.log(pokeName);
+
+    let pokeHeader = document.createElement('h1');
+    pokeHeader.innerText = pokeName;
+    section.appendChild(pokeHeader);
+
+    let pokeType = json.types;
+    
+    let pokeTypestring = ''
+    for (let type of pokeType) {
+        console.log(type)
+        pokeTypestring += `${type.type.name}, `
+    }
+    // if(pokeType.length > 1) {
+        
+    // } else {
+    //     console.log(pokeType)
+    // }
+    console.log(pokeType);
+
+    let pokeData = document.createElement('h2');
+    pokeData.innerText = pokeTypestring;
+    section.appendChild(pokeData);
+
+    // Create an image tag and add the sprite url as the source
+    let pokeSpritestring = ''
+    for (let sprite of pokeSpritestring) {
+        console.log(Sprite)
+        pokeTypestring += `${sprite.sprite.img}, `
+    }
+ 
 }
